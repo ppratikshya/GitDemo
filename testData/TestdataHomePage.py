8cc00034fcefcb1570eb8ff4bb989bff74e77321
pratikshya <preeti.pratikshya51@gmail.com>
2021-05-29 20:38:10 +0530

    new branch

--- testData/TestdataHomePage.py
+++ testData/TestdataHomePage.py
@@ -3,3 +3,7 @@ class TestdataHomePage:
     testdatainput= [{'firstname':'Rahul','email':'Shetty','gender':'Male'}]
 
     testdataimput2=[{'firstname':'Rahul','email':'Shetty','gender':'Male'}]
+
+    testdataimput4=[{'firstname':'Rahul','email':'Shetty','gender':'Male'}]
+
+
